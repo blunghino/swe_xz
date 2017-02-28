23e1f7c1972da89d87f2ecc351455bb9c8685d4c
Brent Lunghino <brentlunghino@gmail.com>
2017-02-28 12:31:21 -0800

    code skeleton done

---
+++ p3_hw5.py
@@ -0,0 +1,36 @@
+import numpy as np 
+
+from swe_xz import *
+
+
+def initial_condition(x, L, a=0.01):
+    return a * np.cos(np.pi * x / L)
+
+def part3():
+    ## parameters/settings
+    N_i = 20
+    N_k = 20
+    L = 1
+    D = 1
+    k = np.pi / L # wave number = 2 pi / lambda (lambda = 2L)
+    c = np.sqrt((9.81/k)*np.tanh(k*D))
+    T = 2 * np.pi / (c*k)
+    t_max = 2 * T
+    Dt = T / 20
+    theta = 0.5
+    ## create problem
+    prob = SweXZProblem(
+        L,
+        D,
+        t_max,
+        N_i,
+        N_k,
+        Dt,
+        theta,
+    )
+    prob.set_h_initial(initial_condition(prob.xc, L))
+    ## run model
+    run_swe_xz_problem(prob)
+
+if __name__ == '__main__':
+    part3()
--- swe_xz.py
+++ swe_xz.py
@@ -2,15 +2,16 @@
 solve the 2D-V shallow water equations
 """
 import numpy as np 
+import scipy.linalg 
 
 
 class SweXZProblem:
     """
     class to store parameters for swe_xz model run
     """
-    def __init__(self, L, D, t_max, N_i, N_k, N_t, h_initial=None):
+    def __init__(self, L, D, t_max, N_i, N_k, Dt, h_initial=None):
         """
-        set default parameters
+        set parameter values
         """
         ## domain length
         self.L = L
@@ -22,33 +23,134 @@ class SweXZProblem:
         self.N_i = N_i
         ## number of grid cells in vertical
         self.N_k = N_k
-        ## number of timesteps
-        self.N_t = N_t
+        ## timestep size
+        self.Dt = Dt
         ## x grid cell size
         self.Dx = L / N_i
         ## z grid cell size
         self.Dz = D / N_k
         ## timestep size
-        self.Dt = t_max / N_t
+        self.N_t = t_max / Dt 
         ## cell centered grid x, z
-        self.xc = np.arrange(self.Dx/2, L + self.Dx/2, self.Dx)
-        self.zc = np.arrange(self.Dz/2, D + self.Dz/2, self.Dz)
-        ## meshed cell centered grid
-        self.Xc, self.Zc = np.meshgrid(self.xc, self.zc)
+        self.xc = np.arange(self.Dx/2, L + self.Dx/2, self.Dx)
+        self.zc = np.arange(self.Dz/2, D + self.Dz/2, self.Dz)
         ## timesteps
-        self.t = np.arrange(0, t_max+self.Dt, self.Dt)
+        self.t = np.arange(0, t_max+Dt, Dt)
+        ## bathymetry
+        self.d = D * np.ones_like(self.xc)
         ## free surface initial condition
         if h_initial is None:
+            ## vector of same length as cell centered x grid
             self.h_initial = np.zeros_like(self.xc)
         else:
             self.h_initial = h_initial
 
+    def set_h_initial(self, h_initial):
+        self.h_initial = h_initial
 
+def calc_S():
+    """
+    S is an N_i by N_k matrix containing explicit data
+    for the u velocity field update 
+    """
+    pass
+
+def calc_RHS_freesurface():
+    """
+    R is a length N_i vector containing the RHS for the
+    free surface height update
+    """
+    pass
+
+def construct_LHS_freesurface():
+    """
+    L is a matrix of shape n_c x n_c where n_c = N_i * N_k 
+    """
+    pass
+
+def predict_u_velocity(q):
+    """
+    """
+    pass
+
+def predict_w_velocity(q):
+    """
+    """
+    pass
+
+def calc_RHS_pressure():
+    """
+    """
+    pass
+
+def construct_LHS_pressure():
+    """
+    """
+    pass
+
+def CG_solve(LHS, RHS):
+    """
+    """
+    pass 
+
+def update_u_velocity(q):
+    """
+    """
+    pass
+
+def update_w_velocity(q):
+    """
+    """
+    pass
 
 def run_swe_xz_problem(p):
     """
     steps to compute solution to `p`, a SweXZProblem
+
+    all vectorized grids are stored row major 
     """
-    u = np.zeros(p.N_i+1)
-    w = np.zeros(p.N_k+1)
+    ## number of points to store for u and w velocity fields
+    n_f = (p.N_i + 1) * (p.N_k + 1)
+    ## number of grid points to store for q non-hydrostatic pressure field
+    n_c = p.N_i * p.N_k 
+    ## initialize vectors
     h = p.h_initial.copy()
+    u = np.zeros(n_f)
+    w = np.zeros(n_f)
+    q = np.zeros(n_c)
+    u_str = np.zeros_like(u)
+    w_str = np.zeros_like(w)
+    q_c = np.zeros_like(q)
+    u_new = np.zeros_like(u)
+    w_new = np.zeros_like(w)
+    q_new = np.zeros_like(q)
+    h_new = np.zeros_like(h)
+    ## time loop starting at t_ = p.Dt
+    for t_ in p.t[1:]:
+        ## cell centered H
+        H_c = h + p.d
+        ## explicit update data for u (a)
+        S = calc_S()
+        ## LHS and RHS for free surface system (b)
+        RHS_h = calc_RHS_freesurface()
+        LHS_h = construct_LHS_freesurface()
+        ## solve for free surface (c)
+        h_new = scipy.linalg.solve(LHS_h, RHS_h)
+        ## update predictor velocity fields using old q (d)
+        u_str = predict_u_velocity(q)
+        w_str = predict_w_velocity(q)
+        ## LHS, RHS for pressure field
+        RHS_q = calc_RHS_pressure()
+        LHS_q = construct_LHS_pressure()
+        ## use CG to solve for q
+        q_c = CG_solve(LHS_q, RHS_q)
+        ## correct predicted u and w velocity fields
+        u_new = update_u_velocity()
+        w_new = update_w_velocity()
+        ## update nonhydrostatic pressure
+        q += q_c
+
+
+
+
+
